ed55f050651d596ece8f829325fb81e3191edb2d
Brian Chao <mathbrian@outlook.com>
2019-09-13 16:22:48 +0800

    最終版

--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -16,7 +16,6 @@ namespace ImageResizer
 
             var imageProcess = new ImageProcess();
             var imageProcessAsync1 = new ImageProcessAsync1();
-            var imageProcessAsync2 = new ImageProcessAsync2();
 
             imageProcess.Clean(destinationPath);
 
@@ -24,24 +23,18 @@ namespace ImageResizer
             sw.Start();
             imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
             sw.Stop();
-
+            var timeSync = sw.ElapsedMilliseconds;
             Console.WriteLine($"同步版花費時間: {sw.ElapsedMilliseconds} ms");
 
             sw.Reset();
             sw.Start();
             imageProcessAsync1.ResizeImagesAsync(sourcePath, destinationPath, 2.0).Wait();
             sw.Stop();
-
+            var timeAsync = sw.ElapsedMilliseconds;
             Console.WriteLine($"非同步版花費時間: {sw.ElapsedMilliseconds} ms");
 
-
-            sw.Reset();
-            sw.Start();
-            imageProcessAsync2.ResizeImagesAsync(sourcePath, destinationPath, 2.0).Wait();
-            sw.Stop();
-
-            Console.WriteLine($"非同步版2花費時間: {sw.ElapsedMilliseconds} ms");
-
+            var comparison = ((double)(timeSync - timeAsync) / timeSync) * 100;
+            Console.WriteLine($"節省: {comparison:N2} % 作業時間");
 
             Console.ReadKey();
         }
